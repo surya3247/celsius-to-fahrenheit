44f4eb0a3df76b8d25c3fa8649415930ae4888a8
surya3247 <142500272+surya3247@users.noreply.github.com>
2023-09-15 23:28:33 +0530

    celtofer

---
+++ Celc-Fehr/cal.js
@@ -0,0 +1,10 @@
+function conert(){
+    let cmval = Number(document.getElementById("input").value)
+    let inchval =(cmval*9/5)+32;
+    let result=document.getElementById("result").innerHTML=inchval.toFixed(2) + "°F "
+}
+
+//  celcius to fahrehit
+// function coverts(){
+//     let far =
+// }
